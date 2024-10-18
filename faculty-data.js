e342f0ae311dd6dc710be74ae3a8235d6458ea13
Arjun gupta <rachnag352@gmail.com>
2024-10-18 21:48:57 +0530

    Done About-us and about facaulty

--- faculty-data.js
+++ faculty-data.js
@@ -4,69 +4,69 @@ const facultyData = [
         qualification: "Ph.D., M.E.",
         phone: "+91-1672-253208",
         email: "birmohansingh@sliet.ac.in",
-        photo: "https://via.placeholder.com/300x200?text=Dr.+Birmohan+Singh"
+        photo: "./assets/faculty_images/birmohan.jpg"
     },
     {
         name: "Dr. Damanpreet Singh",
         qualification: "Ph.D., M.Tech, B.Tech.",
         phone: "+91-1672-253210",
         email: "damanpreets@sliet.ac.in",
-        photo: "https://via.placeholder.com/300x200?text=Dr.+Damanpreet+Singh"
+        photo: "./assets/faculty_images/damanpreet.jpg"
     },
     {
         name: "Dr. Major Singh Goraya",
         qualification: "Ph.D., M.Tech., B.Tech.",
         phone: "01672-253212",
         email: "mjrsingh@yahoo.com",
-        photo: "https://via.placeholder.com/300x200?text=Dr.+Major+Singh+Goraya"
+        photo: "./assets/faculty_images/Major.jpg"
     },
     {
         name: "Dr. Manoj Sachan",
         qualification: "B.Tech(Computer Science), M.E(Computer Science), Ph.D (Computer Science & Engineering)",
         phone: "+91-1672-253214",
         email: "manojsachan@sliet.ac.in",
-        photo: "https://via.placeholder.com/300x200?text=Dr.+Manoj+Sachan"
+        photo: "./assets/faculty_images/Manoj.jpg"
     },
     {
         name: "Dr. Gurjinder Kaur",
         qualification: "B.E. Kyrgyz Technical University, Bishkek M.S. BITS, Pilani Ph.D SLIET, Longowal",
         phone: "+91-1672-253326",
         email: "gurjinder13@yahoo.com",
-        photo: "https://via.placeholder.com/300x200?text=Dr.+Gurjinder+Kaur"
+        photo: "./assets/faculty_images/gur.jpg"
     },
     {
         name: "Dr. Amar Nath",
         qualification: "Ph.D. in CSE from IIT Roorkee, M. Tech. in CSE from NIT Rourkela, B. Tech. in CSE from BIET Jhansi",
         phone: "01672-253610",
         email: "amarnath@sliet.ac.in",
-        photo: "https://via.placeholder.com/300x200?text=Dr.+Amar+Nath"
+        photo: "./assets/faculty_images/amar.jpg"
     },
     {
         name: "Dr. Jagdeep Singh",
         qualification: "Ph.D., M. Tech, B. Tech in Computer Science & Engineering",
         phone: "01672-253320",
         email: "jagdeep@sliet.ac.in",
-        photo: "https://via.placeholder.com/300x200?text=Dr.+Jagdeep+Singh"
+        photo: "./assets/faculty_images/jagdeep.jpg"
     },
     {
         name: "Dr. Manminder Singh",
         qualification: "B.Tech, M.Tech, Ph. D (Computer Science & Engineering)",
         phone: "+91-01672-253328",
         email: "manmindersingh@sliet.ac.in",
-        photo: "https://via.placeholder.com/300x200?text=Dr.+Manminder+Singh"
+        photo: "./assets/faculty_images/man.jpg"
     },
     {
         name: "Dr. Preetpal Kaur Buttar",
         qualification: "PhD (Computer Science & Engineering)",
         phone: "01672-253614",
         email: "preetpal@sliet.ac.in",
-        photo: "https://via.placeholder.com/300x200?text=Dr.+Preetpal+Kaur+Buttar"
+        photo: "./assets/faculty_images/preet.jpg"
     },
     {
         name: "Dr. Tajinder Singh",
         qualification: "Ph.D in Machine Learning Based Text Mining in Social Media from NIT Hamirpur",
         phone: "91-6283963279",
         email:  "tajindersingh@sliet.ac.in",
-        photo: "https://via.placeholder.com/300x200?text=Dr.+Tajinder+Singh"
+        photo: "./assets/faculty_images/tajj.jpg"
     }
 ];
