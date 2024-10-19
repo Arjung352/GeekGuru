67cdb55fd6b0f605a33ecafef507156f3b2e4611
Arjun gupta <rachnag352@gmail.com>
2024-10-19 11:46:14 +0530

    Final push

--- faculty-data.js
+++ faculty-data.js
@@ -68,5 +68,87 @@ const facultyData = [
         phone: "91-6283963279",
         email:  "tajindersingh@sliet.ac.in",
         photo: "./assets/faculty_images/tajj.jpg"
+    },
+    {
+        "name": "Dr. Utkarsh",
+        "qualification": "Ph.d, School of Computer & Systems Sciences,JNU ; M.TECH ,JNU ; B.TECH (IT), UIET CSJM University",
+        "phone": "91-XXXXXXXXXX",
+        "email": "utkarsh@example.com",
+        "photo": "./assets/faculty_images/utkarsh.jpg"
+    },
+    {
+        "name": "Dr. Vinod Kumar Verma",
+        "qualification": "Ph.D. Computer Science and Engineering",
+        "phone": "+91-1672-253218",
+        "email": "vinod5881@gmail.com",
+        "photo": "./assets/faculty_images/vinod.jpg"
+    },
+    {
+        "name": "Jaspal Singh",
+        "qualification": "M.E. (CSE & IT), Pursuing PhD in Cloud Computing",
+        "phone": "01672-253327",
+        "email": "safrisoft@yahoo.com, jaspalsingh@sliet.ac.in",
+        "photo": "./assets/faculty_images/jaspal.jpg"
+    },
+    {
+        "name": "Rahul Gautam",
+        "qualification": "Ph.D. pursuing, M.Tech (Computer Science and Applications) from Thapar University, Patiala; B.Tech (Computer Science and Info. Technology) from Sir Chhotu Ram Inst. of Engg. & Technology, CCS University Campus, Meerut.",
+        "phone": "+91-1672-253612",
+        "email": "rahulgautam@sliet.ac.in",
+        "photo": "./assets/faculty_images/rahul.jpg"
+    },
+    {
+        "name": "Dr. Jatinder Pal Singh",
+        "qualification": "B.Tech - Computer Science and Engineering, M.Tech - Computer Science and Engineering, Ph.D. - Computer Science and Engineering",
+        "email": "sachdeva.jp@gmail.com, jatinderpalsingh@sliet.ac.in",
+        "photo": "./assets/faculty_images/jatinder.jpg"
+    },
+    {
+        "name": "Sukhpreet Singh",
+        "qualification": "B.Tech, M.Tech, Ph.D (Pursuing)",
+        "email": "sukhpreet.manshahia@gmail.com",
+        "photo": "./assets/faculty_images/sukhpreet.jpg"
+    },
+    {
+        "name": "Abhishek",
+        "qualification": "Not available",
+        "phone": "Not available",
+        "email": "Not available",
+        "photo": "./assets/faculty_images/noimg.png"
+    },
+    {
+        "name": "Chinu",
+        "qualification": "Ph.D. (Pursuing), M.Tech, B.Tech",
+        "phone": "9915025805",
+        "email": "chinu@sliet.ac.in",
+        "photo": "./assets/faculty_images/chinu.jpg"
+    },
+    {
+        "name": "Dr. Vikash Kumar Garg",
+        "qualification": "B.Tech(IT), M.Tech(Computer Science & Engineering), Ph.D (Big Data, Hadoop, MongoDB) (Computer Science & Engineering)",
+        "phone": "9988610629",
+        "email": "contactvikashgarg@gmail.com",
+        "photo": "./assets/faculty_images/vikash.jpg"
+    },
+    {
+        "name": "Harpreet Singh",
+        "qualification": "Not available",
+        "phone": "Not available",
+        "email": "Not available",
+        "photo": "./assets/faculty_images/noimg.png"
+    },
+    {
+        "name": "Sakshi Bansal",
+        "qualification": "Not available",
+        "phone": "Not available",
+        "email": "Not available",
+        "photo": "./assets/faculty_images/noimg.png"
+    },
+    {
+        "name": "Sunita Rani",
+        "qualification": "Ph.D(Pursuing), M.Tech, B.Tech",
+        "phone": "8196013222",
+        "email": "sunitarani@sliet.ac.in",
+        "photo": "./assets/faculty_images/sunita.jpg"
     }
 ];
